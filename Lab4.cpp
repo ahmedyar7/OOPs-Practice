bec8389e93012de6aff2fa06e48d7ffd7e38a68d
ahmedyar7 <ahmedyar778@gmail.com>
2024-03-06 15:20:47 +0500

    Lab 3

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,131 +1,131 @@
 // 1.	Create a class named TemperatureConverter with overloaded functions to convert temperatures between Celsius and Fahrenheit. Implement functions for converting Celsius to Fahrenheit and vice versa. Allow users to input temperatures and display the converted results.
 
-// #include <iostream>
-// using namespace std;
-// class TemperatureConverter
-// {
-// public:
-//     double celsiusToFahrenheit(double celsius)
-//     {
-//         return (celsius * 9 / 5) + 32;
-//     }
+#include <iostream>
+using namespace std;
+class TemperatureConverter
+{
+public:
+    double celsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
 
-//     double FahrenheitTocelsius(double fahrenheit)
-//     {
-//         return (fahrenheit - 32) * 5 / 9;
-//     }
-// };
+    double FahrenheitTocelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+};
 
-// int main()
-// {
-//     TemperatureConverter tempConverter;
-//     float temperature;
+int main()
+{
+    TemperatureConverter tempConverter;
+    float temperature;
 
-//     cout << "Enter the Temperature: ";
-//     cin >> temperature;
+    cout << "Enter the Temperature: ";
+    cin >> temperature;
 
-//     char choice;
-//     cout << "Enter the Choice: ";
-//     cin >> choice;
+    char choice;
+    cout << "Enter the Choice: ";
+    cin >> choice;
 
-//     if (choice == 'C' || choice == 'c')
-//     {
-//         cout << "Celsius To Fahrenhiet: " << tempConverter.celsiusToFahrenheit(temperature) << endl;
-//     }
-//     else if (choice == 'F' || choice == 'f')
-//     {
-//         cout << "Fahrenheit to Celsius: " << tempConverter.FahrenheitTocelsius(temperature) << endl;
-//     }
-// }
+    if (choice == 'C' || choice == 'c')
+    {
+        cout << "Celsius To Fahrenhiet: " << tempConverter.celsiusToFahrenheit(temperature) << endl;
+    }
+    else if (choice == 'F' || choice == 'f')
+    {
+        cout << "Fahrenheit to Celsius: " << tempConverter.FahrenheitTocelsius(temperature) << endl;
+    }
+}
 
 // 2.	Design a class MathOperations with overloaded functions for basic math operations such as addition, subtraction, multiplication, and division. The functions should handle both integers and doubles. Allow users to input numbers and choose the operation to perform
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class MathOperations
-// {
-// public:
-//     double add(int a, int b)
-//     {
-//         return a + b;
-//     }
-//     double add(double a, double b)
-//     {
-//         return a + b;
-//     }
+class MathOperations
+{
+public:
+    double add(int a, int b)
+    {
+        return a + b;
+    }
+    double add(double a, double b)
+    {
+        return a + b;
+    }
 
-//     double subtract(int a, int b)
-//     {
-//         return a - b;
-//     }
-//     double subtract(double a, double b)
-//     {
-//         return a - b;
-//     }
+    double subtract(int a, int b)
+    {
+        return a - b;
+    }
+    double subtract(double a, double b)
+    {
+        return a - b;
+    }
 
-//     double multiply(int a, int b)
-//     {
-//         return a * b;
-//     }
-//     double multiply(double a, double b)
-//     {
-//         return a * b;
-//     }
+    double multiply(int a, int b)
+    {
+        return a * b;
+    }
+    double multiply(double a, double b)
+    {
+        return a * b;
+    }
 
-//     double divide(int a, int b)
-//     {
-//         if (b == 0)
-//         {
-//             cout << "Please Provide Valid input \n";
-//         }
-//         return a / b;
-//     }
-//     double divide(double a, double b)
-//     {
-//         if (b == 0.0)
-//             cout << "Please Provide Valid Input: \n";
-//         return a / b;
-//     }
-// };
+    double divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            cout << "Please Provide Valid input \n";
+        }
+        return a / b;
+    }
+    double divide(double a, double b)
+    {
+        if (b == 0.0)
+            cout << "Please Provide Valid Input: \n";
+        return a / b;
+    }
+};
 
-// int main()
-// {
-//     MathOperations math;
-//     double a, b;
-//     cout << "Enter first Number: ";
-//     cin >> a;
-//     cout << "Enter 2nd Number: ";
-//     cin >> b;
+int main()
+{
+    MathOperations math;
+    double a, b;
+    cout << "Enter first Number: ";
+    cin >> a;
+    cout << "Enter 2nd Number: ";
+    cin >> b;
 
-//     char operators;
-//     cout << "Enter The Operater: (+, -, /, *)  \n";
-//     cin >> operators;
+    char operators;
+    cout << "Enter The Operater: (+, -, /, *)  \n";
+    cin >> operators;
 
-//     if (operators == '+')
-//     {
-//         cout << "Addition Result: " << math.add(a, b) << endl;
-//     }
+    if (operators == '+')
+    {
+        cout << "Addition Result: " << math.add(a, b) << endl;
+    }
 
-//     else if (operators == '-')
-//     {
-//         cout << "Subtraction Result: " << math.subtract(a, b) << endl;
-//     }
+    else if (operators == '-')
+    {
+        cout << "Subtraction Result: " << math.subtract(a, b) << endl;
+    }
 
-//     else if (operators == '*')
-//     {
-//         cout << "Multiplication Result: " << math.multiply(a, b) << endl;
-//     }
+    else if (operators == '*')
+    {
+        cout << "Multiplication Result: " << math.multiply(a, b) << endl;
+    }
 
-//     else if (operators == '/')
-//     {
-//         cout << "Division Result: " << math.divide(a, b) << endl;
-//     }
-//     else
-//         cout << "Please Provide valid input \n";
+    else if (operators == '/')
+    {
+        cout << "Division Result: " << math.divide(a, b) << endl;
+    }
+    else
+        cout << "Please Provide valid input \n";
 
-//     return 0;
-// }
+    return 0;
+}
 
 // 3.	Create a class named StringManipulator with overloaded functions for string manipulation. Implement functions to concatenate two strings, find the length of a string, and convert a string to uppercase. Allow users to input strings and choose the operation to perform.
 
