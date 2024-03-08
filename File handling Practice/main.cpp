552aef6c95c1939828af816d8951ec1ec4ce33c7
ahmedyar7 <ahmedyar778@gmail.com>
2024-03-08 19:19:07 +0500

    Basic file reading and writing in cpp

--- File handling Practice/main.cpp
+++ File handling Practice/main.cpp
@@ -1,30 +1,54 @@
-// Write a C++ program that reads a list of integers from a file and calculates their sum
+// // Write a C++ program that reads a list of integers from a file and calculates their sum
 
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-// int main()
-// {
-//     ifstream file("example.txt");
-
-//     int numbers;
-//     int sum = 0;
-
-//     while (file >> numbers)
-//     {
-//         sum += numbers;
-//     }
-//     cout << sum << endl;
-// }
-
 int main()
 {
     ifstream file("example.txt");
 
-    if (!file)
+    int numbers;
+    int sum = 0;
+
+    while (file >> numbers)
     {
-        cerr << "Error Opening the file. " << endl;
-        return 1;
+        sum += numbers;
+    }
+    cout << sum << endl;
+}
+
+// Program to calculate the average grade of the student and then write the grade in to another file from the computer
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 2};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        sum += array[i];
+    }
+    // Writing the the file
+    ofstream in("example.txt");
+    in << sum;
+    cout << sum << endl;
+}
+
+// Printing name from the file
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    string str;
+    ifstream file("example.txt");
+    while (getline(file, str))
+    {
+        cout << str << endl;
     }
 }
