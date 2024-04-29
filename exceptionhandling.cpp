3e949cb220718eb809dd9fd048e8cbe99ec3bfcc
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-29 23:39:04 +0500

    exception handinling in cpp examples in the actual program

--- exceptionhandling.cpp
+++ exceptionhandling.cpp
@@ -1,24 +1,54 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int age = 20;
+//     try
+//     {
+//         if (age >= 24)
+//         {
+//             cout << "You have the access to drink the alcohol";
+//         }
+//         else
+//         {
+//             throw(age);
+//         }
+//     }
+//     catch (int age)
+//     {
+//         cout << "you donot have the access to drink the alcohol" << endl;
+//         cout << "You age is " << age << endl;
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int age = 20;
+    int num;
+    int denum;
+    int output;
+
+    cout << "Enter the number for the divison: ";
+    cin >> num;
+    cin >> denum;
+
     try
     {
-        if (age >= 24)
+        if (denum == 0)
         {
-            cout << "You have the access to drink the alcohol";
+            throw denum;
         }
         else
         {
-            throw(age);
+            output = num / denum;
         }
     }
-    catch (int age)
+    catch (int d)
     {
-        cout << "you donot have the access to drink the alcohol" << endl;
-        cout << "You age is " << age << endl;
+        cout << "You cannot enter zero in denominator beacause this will give the undefined terms \n";
     }
-    return 0;
 }
