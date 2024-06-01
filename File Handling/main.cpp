6a77e48ff03ae573414b51c0874fcb8704da39be
ahmedyar7 <ahmedyar778@gmail.com>
2024-06-01 14:34:20 +0500

    Getting started with the file handling in C++

--- File Handling/main.cpp
+++ File Handling/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 class FileHandling
@@ -45,11 +46,114 @@ public:
         writeFile << "\nWord Count: " << word_count << endl;
         writeFile << "\nCharacter Count: " << char_count << endl;
     }
+
+    void numberofwordnumberofcharacters()
+    {
+        // TODO: Reading from the file
+        ifstream readFile("file.txt");
+        if (!readFile.is_open())
+            cout << "Could not open the file " << endl;
+
+        string line;
+        int w_count = 0;
+        int c_count = 0;
+
+        while (getline(readFile, line))
+        {
+            bool inword = false;
+            for (char c : line)
+            {
+                if (isspace(c))
+                {
+                    w_count++;
+                    inword = true;
+                }
+                else
+                {
+                    c_count++;
+                    inword = false;
+                }
+            }
+            if (inword)
+            {
+                c_count++;
+            }
+        }
+        ofstream write_file("file.txt", ios::app);
+        cout << endl;
+        write_file << "Word count: " << w_count << endl;
+        write_file << "Character count: " << c_count << endl;
+        // TODO: Applying the algorithm
+        // TODO: Writing to the file
+    }
+    void reversingOfWord()
+    {
+        string userInput;
+        cout << "Enter the string: " << endl;
+        getline(cin, userInput);
+
+        stringstream ss(userInput);
+        string word;
+        string reversedWord;
+
+        while (ss >> word)
+        {
+            reversedWord = word + "" + reversedWord;
+        }
+        cout << reversedWord << endl;
+    }
+
+    void sortingNumbers()
+    {
+        ifstream readFile("file.txt");
+        if (!readFile.is_open())
+        {
+            cout << "This file could not open" << endl;
+        }
+        int i;
+        vector<int> numbers;
+        while (readFile >> i)
+        {
+            numbers.push_back(i);
+        }
+        if (numbers.empty())
+            cout << "The file is empty" << endl;
+
+        for (int i = 0; i < numbers.size() - 1; i++)
+        {
+            for (int j = i + 1; j < numbers.size(); j++)
+            {
+                if (numbers[j] < numbers[i])
+                {
+                    int swap = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = swap;
+                }
+            }
+        }
+        ofstream writeFile("file.txt", ios::app);
+        if (!writeFile.is_open())
+        {
+            cout << "Could not open this file" << endl;
+        }
+
+        cout << endl;
+        writeFile << "Sorted Array: " << endl;
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            writeFile << numbers[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
 int main()
 {
     FileHandling f;
-    f.num_word_num_char();
+    f.sortingNumbers();
+    // f.reversingOfWord();
+    // f.numberofwordnumberofcharacters();
+    // f.num_word_num_char();
+
     return 0;
 }
