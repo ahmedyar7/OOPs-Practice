280dc3ad721e8e8acee685c303ab979806912c81
ahmedyar7 <ahmedyar778@gmail.com>
2024-06-02 14:09:16 +0500

    Adding the seriszeationat oin tin the gitpu

--- Serialization/main.cpp
+++ Serialization/main.cpp
@@ -30,7 +30,7 @@ public:
     void deserialize(ifstream &in)
     {
         size_t name_length;
-        in.read(reinterpret_cast<char *>(name_length), sizeof(name_length));
+        in.read(reinterpret_cast<char *>(&name_length), sizeof(name_length));
         char *buffer = new char[name_length + 1];
         in.read(buffer, name_length);
         buffer[name_length] = '\0';
@@ -39,33 +39,67 @@ public:
         in.read(reinterpret_cast<char *>(&age), sizeof(age));
     }
 };
+
+class Man
+{
+public:
+    string name;
+    int age;
+
+    Man(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+    Man() {}
+
+    void serialize(ofstream &out)
+    {
+        size_t name_len = name.size();
+        out.write(reinterpret_cast<const char *>(&name_len), sizeof(name));
+        out.write(name.c_str(), name_len);
+        out.write(reinterpret_cast<const char *>(&age), sizeof(age));
+    }
+    void deserialize(ifstream &in)
+    {
+        size_t name_len;
+        in.read(reinterpret_cast<char *>(&name_len), sizeof(name_len));
+        char *buffer = new char[name_len + 1];
+        in.read(buffer, name_len);
+        buffer[name_len] = '\0';
+        name = buffer;
+        delete[] buffer;
+        in.read(reinterpret_cast<char *>(&age), sizeof(age));
+    }
+};
+
 int main()
 {
-    Person p1("Ahmed", 30);
-
-    // Serialze object to the file.
-    ofstream writeFile("file.txt", ios::binary);
-    if (writeFile.is_open())
+    Man m1("Ahmed", 23);
+    // Serialize the object
+    ofstream outFile("file.dat", ios::binary);
+    if (outFile.is_open())
     {
-        p1.serialize(writeFile);
-        writeFile.close();
+        m1.serialize(outFile);
+        outFile.close();
     }
     else
     {
         cout << "Could not open the file" << endl;
     }
 
-    // Deserialze the object;
-    Person p2("Yar", 80);
-    ifstream readFile("file.txt", ios::binary);
-    if (readFile.is_open())
+    Man m2("Yar", 223);
+    // Serialize the object
+    ifstream inFile("file.dat", ios::binary);
+    if (inFile.is_open())
     {
-        p2.deserialize(readFile);
-        readFile.close();
+        m2.deserialize(inFile);
+        inFile.close();
     }
     else
     {
         cout << "Could not open the file" << endl;
     }
-    cout << "Name: " << p2.name << " Age: " << p2.age;
+    cout << "Name: " << m2.name << endl;
+    cout << "Age: " << m2.age << endl;
 }
