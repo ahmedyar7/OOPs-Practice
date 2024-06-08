72a675b5a4c0195aeb5d443f6ec411ba5b353bd4
ahmedyar7 <ahmedyar778@gmail.com>
2024-06-08 20:12:39 +0500

    Locking and mutex mechanism in the multithreading in C++

--- MultiThreading/code.cpp
+++ MultiThreading/code.cpp
@@ -3,20 +3,41 @@
 #include <thread>
 using namespace std;
 
-void function_1()
-{
-    cout << "This is function 1" << endl;
-}
-void function_2()
-{
-    cout << "This is function 2" << endl;
-}
+// void function_1()
+// {
+//     cout << "This is function 1" << endl;
+// }
+// void function_2()
+// {
+//     cout << "This is function 2" << endl;
+// }
 
+// int main()
+// {
+//     thread t1(function_1);
+//     thread t2(function_2);
+
+//     t1.join();
+//     t2.join();
+// }
+
+mutex mux;
+void threads_task()
+{
+    int shared_variable = 0;
+    lock_guard<mutex> lock(mux);
+    shared_variable++;
+    cout << "Value of Shared Variable: " << shared_variable << endl;
+}
 int main()
 {
-    thread t1(function_1);
-    thread t2(function_2);
-
-    t1.join();
-    t2.join();
+    thread threads[5];
+    for (int i = 0; i < 5; i++)
+    {
+        threads[i] = thread(threads_task);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        threads[i].join();
+    }
 }
